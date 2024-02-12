81babc1bdf69df12f3d41717e349512ef594c244
Thomas <Touhma@outlook.com>
2024-02-12 11:50:58 -0500

    V0.1 Of the website

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "The Playtypus Experiment #0",
+        "excerpt":"So It Begin … What is this game about ? Short Version A factory game in space. Longer Version Nothing being set in stone YET as we are still early in the project what I can share for now is that the game will take inspirations from a little bit...","categories": ["Announcements"],
+        "tags": [],
+        "url": "/announcements/2024/02/12/The-Playtypus-Experiment-0.html",
+        "teaser": null
+      }]
